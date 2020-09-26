15e2df9bd43d7fb29877325fe9b441897b8a197d
jomagsm <jomagsm@gmail.com>
2020-09-26 14:18:43 +0600

    Дописал 2 этап

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 
-from webapp.views.product_view import IndexView, ProductView
+from webapp.views.product_view import IndexView, ProductView, ProductCreateView, ProductUpdateView, ProductDeleteView
 
 app_name = 'webapp'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('product/<int:pk>/', ProductView.as_view(), name='product_view'),
+    path('product_create/', ProductCreateView.as_view(), name='product_create'),
+    path('product_update/<int:pk>', ProductUpdateView.as_view(), name='product_update'),
+    path('product_delete/<int:pk>', ProductDeleteView.as_view(), name='product_delete')
 ]
--- source/webapp/views/product_view.py
+++ source/webapp/views/product_view.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.core.paginator import Paginator
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
 
+from webapp.forms import ProductForm
 from webapp.models import Product
 
 
@@ -41,3 +44,32 @@ class ProductView(DetailView):
             return page.object_list, page, is_paginated
         else:
             return reviews, None, False
+
+
+class ProductCreateView(PermissionRequiredMixin, CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'product/product_create.html'
+    permission_required = 'webapp.add_product'
+
+    def get_success_url(self):
+        return reverse('webapp:product_view', kwargs={'pk': self.object.pk})
+
+
+class ProductUpdateView(PermissionRequiredMixin,UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'product/product_update.html'
+    permission_required = 'webapp.change_product'
+
+    def get_success_url(self):
+        return reverse('webapp:product_view', kwargs={'pk': self.object.pk})
+
+
+class ProductDeleteView(PermissionRequiredMixin, DeleteView):
+    model = Product
+    template_name = 'product/product_delete.html'
+    success_url = reverse_lazy('webapp:index')
+    permission_required = 'webapp.delete_product'
+
+
